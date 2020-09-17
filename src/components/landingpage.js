ae9ebe7ab2400bcad15d04058bf489c3c0e3fe8c
KekeliVa <kekeli.afantchao@yahoo.com>
2020-09-17 16:44:25 -0400

    built out most of landing page, will need to edit spacing between font-awesome icons

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react"
 import { Grid, Cell } from "react-mdl"
+{/* import image from "../_DSC0717.jpg" */}
 
 class LandingPage extends Component {
   render() {
@@ -7,10 +8,25 @@ class LandingPage extends Component {
       <div style={{width: "100%", margin: "auto"}}>
         <Grid className="landing-grid">
           <Cell col={12}></Cell>
-            <img src= "../public/_DSC0717.jpg"/>
+            {/* <img src= {image}/> */}
+            
             <div className="banner-text">
               <h1>Full Stack Web Developer</h1>
+              <p>HTML/CSS | JavaScript | React | MySQL | Express | NodeJS | Bootstrap</p>
+            
+              <div className= "socials">
+                {/*LinkedIn */}
+                <a href="https://www.linkedin.com/in/afantchao/" rel="noopener noreferrer" target="_blank">
+                  <i class="fab fa-linkedin-in" aria-hidden="true"></i>
+                </a>
+
+                {/*GitHub */}
+                <a href="https://github.com/KekeliVA" rel="noopener noreferrer" target="_blank">
+                  <i class="fab fa-github-square" aria-hidden="true"></i>
+                </a>
+              </div>             
             </div>
+
         </Grid>
       </div>
     )
